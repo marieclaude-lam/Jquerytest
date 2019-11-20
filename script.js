a3a59d919f11b45dbda48909735f8487511f3462
marie-claude lam <marieclaude.lam@gmail.com>
2019-11-20 10:16:41 +0100

    jquery: ouverture de la description

--- script.js
+++ script.js
@@ -19,6 +19,12 @@ $(function(){
 //pour le moment l'image colorée est à 140px dans le css
 //on va la mettre à 0px pour la faire apparaitre
                 $colorImage.animate({left:"0px"},250);
+//Je veux ouvrir la description dont left est aussi à 140px (css)
+//et son li a aussi pour largeur de 140px c'est pour ça que l'on a que l'image
+//il faut donc augmenter la largeur du li qui est stocké dans $item
+                $item.animate({width:"420px"},250);
+//mise à jour de la var openIndex
+                openedIndex =newIndex;
 
             });
 
